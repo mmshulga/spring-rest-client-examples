96bd75cf2a21cce1f5602c8ec2515922d8c481f2
Mikhail Shulga <mmshulga@gmail.com>
2018-12-07 17:39:58 +0300

    Refactored to use API uri from properties file.

--- src/main/java/guru/springframework/springrestclientexamples/services/impl/ApiServiceImpl.java
+++ src/main/java/guru/springframework/springrestclientexamples/services/impl/ApiServiceImpl.java
@@ -4,24 +4,32 @@ import guru.springframework.api.domain.User;
 import guru.springframework.api.domain.UserData;
 import guru.springframework.springrestclientexamples.services.ApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
 public class ApiServiceImpl implements ApiService {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
+
+    private final String apiUrl;
 
     @Autowired
-    public ApiServiceImpl(RestTemplate restTemplate) {
+    public ApiServiceImpl(RestTemplate restTemplate, @Value("${api.url}") String apiUrl) {
         this.restTemplate = restTemplate;
+        this.apiUrl = apiUrl;
     }
 
     @Override
     public List<User> getUsers(Integer limit) {
-        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        UriComponentsBuilder uriBuilder = UriComponentsBuilder
+                .fromUriString(apiUrl)
+                .queryParam("limit", limit);
+        UserData userData = restTemplate.getForObject(uriBuilder.toUriString(), UserData.class);
         return userData.getData();
     }
 }
