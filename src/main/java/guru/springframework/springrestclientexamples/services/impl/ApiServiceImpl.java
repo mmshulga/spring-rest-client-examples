95744676cb7d1d9e6263e6768665e976df8bbc6d
Mikhail Shulga <mmshulga@gmail.com>
2018-12-07 17:18:03 +0300

    Added a config, a simple service and a test. Also refactored an original model a little.

---
+++ src/main/java/guru/springframework/springrestclientexamples/services/impl/ApiServiceImpl.java
@@ -0,0 +1,27 @@
+package guru.springframework.springrestclientexamples.services.impl;
+
+import guru.springframework.api.domain.User;
+import guru.springframework.api.domain.UserData;
+import guru.springframework.springrestclientexamples.services.ApiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class ApiServiceImpl implements ApiService {
+
+    private RestTemplate restTemplate;
+
+    @Autowired
+    public ApiServiceImpl(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public List<User> getUsers(Integer limit) {
+        UserData userData = restTemplate.getForObject("http://apifaketory.com/api/user?limit=" + limit, UserData.class);
+        return userData.getData();
+    }
+}
